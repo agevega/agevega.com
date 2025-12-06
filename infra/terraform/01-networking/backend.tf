05887f3f513aacd6cb65690d5ba48a2429be4e0c
Alejandro Vega <agevega@gmail.com>
2025-12-06 23:43:14 +0100

    move tfstate s3

--- infra/terraform/01-networking/backend.tf
+++ infra/terraform/01-networking/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-agevegacom"
-    key            = "envs/lab/agevegacom/terraform.tfstate"
+    key            = "envs/lab/agevegacom/01-networking/terraform.tfstate"
     region         = "eu-south-2"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
