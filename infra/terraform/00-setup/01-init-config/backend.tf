88adfa7b3408d3fb937877de7dfa8415704bdaca
Alejandro Vega Vaquero <agevega@hotmail.com>
2025-12-27 17:50:13 +0100

    feat: add Terraform S3 backend configuration

--- infra/terraform/00-setup/01-init-config/backend.tf
+++ infra/terraform/00-setup/01-init-config/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-agevegacom"
-    key            = "envs/lab/agevegacom/01-init-config/terraform.tfstate"
+    key            = "envs/lab/agevegacom/00-backend-S3/01-init-config/terraform.tfstate"
     region         = "eu-south-2"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
