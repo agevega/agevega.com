58668d4a0bcb632ca6dd83f786fc15884824d4fa
Alejandro Vega Vaquero <agevega@gmail.com>
2026-01-19 00:00:02 +0100

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Default values (Fallback for Local Dev or Error)
-PROVIDER="Localhost"
-REGION="Unknown"
-AZ="Unknown"
-INSTANCE_ID="Unknown"
-INSTANCE_TYPE="Unknown"
-ARCH=$(uname -m) # Use container arch as fallback
+PROVIDER="Local (Simulated)"
+REGION="Local"
+AZ="Unknown (Local/Error)"
+INSTANCE_ID="Unknown (Local/Error)"
+INSTANCE_TYPE="Unknown (Local/Error)"
+ARCH=$(uname -m)
 
 # Try to get AWS IMDSv2 Token (Timeout 2s to not block startup long)
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" -s --connect-timeout 2)
@@ -16,11 +16,11 @@ if [ -n "$TOKEN" ]; then
     PROVIDER="AWS"
     
     # Fetch Metadata
-    REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/region | tr -d '\n\r')
-    AZ=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/availability-zone | tr -d '\n\r')
-    INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-id | tr -d '\n\r')
-    INSTANCE_TYPE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-type | tr -d '\n\r')
-    ARCH=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/architecture | tr -d '\n\r')
+    REGION=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/region | tr -d '\n\r"')
+    AZ=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/availability-zone | tr -d '\n\r"')
+    INSTANCE_ID=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-id | tr -d '\n\r"')
+    INSTANCE_TYPE=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-type | tr -d '\n\r"')
+    ARCH=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/architecture | tr -d '\n\r"')
 fi
 
 # Generate meta.json in Nginx root
