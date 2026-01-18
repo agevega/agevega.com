28724a3f50d954be818cbc20aba80740716f7099
Alejandro Vega Vaquero <agevega@gmail.com>
2026-01-19 00:07:02 +0100

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -20,7 +20,8 @@ if [ -n "$TOKEN" ]; then
     AZ=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/placement/availability-zone | tr -d '\n\r"')
     INSTANCE_ID=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-id | tr -d '\n\r"')
     INSTANCE_TYPE=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/instance-type | tr -d '\n\r"')
-    ARCH=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/architecture | tr -d '\n\r"')
+    ARCH_META=$(curl -f -H "X-aws-ec2-metadata-token: $TOKEN" -s --connect-timeout 2 http://169.254.169.254/latest/meta-data/architecture | tr -d '\n\r"')
+    if [ -n "$ARCH_META" ]; then ARCH="$ARCH_META"; fi
 fi
 
 # Generate meta.json in Nginx root
