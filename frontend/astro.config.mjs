9edde0008c81e268d8b2acbe56652584f65616af
Alejandro Vega Vaquero <agevega@gmail.com>
2026-01-18 18:32:06 +0100

    astro improvement

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  env: {
+    schema: {
+      PUBLIC_APP_VERSION: envField.string({ context: 'client', access: 'public', optional: true, default: 'dev' }),
+    },
+  },
 });
