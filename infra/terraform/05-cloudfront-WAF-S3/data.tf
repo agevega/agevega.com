aef2dab6d7198173929ad09552f791b60ceb7cb8
Alejandro Vega Vaquero <agevega@gmail.com>
2026-01-24 11:56:30 +0100

    Remove CloudFront, WAF, and S3 Terraform module
    Deleted the entire 05-cloudfront-WAF-S3 Terraform module, including all configuration, variables, outputs, and provider files. This removes the infrastructure definitions for CloudFront, WAF, and the associated S3 assets bucket.

--- infra/terraform/05-cloudfront-WAF-S3/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "security" {
-  backend = "s3"
-  config = {
-    bucket  = "agevegacom-terraform-state"
-    key     = "modules/02-bastion-EC2/00-security/terraform.tfstate"
-    region  = "eu-south-2"
-    profile = "terraform"
-  }
-}
