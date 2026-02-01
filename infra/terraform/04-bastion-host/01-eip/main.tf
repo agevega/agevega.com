ae2fcf9cfda4865e08d74e7539179963be2ca02b
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-01 18:06:13 +0100

    bastion eip SSM

--- infra/terraform/04-bastion-host/01-eip/main.tf
+++ infra/terraform/04-bastion-host/01-eip/main.tf
@@ -6,3 +6,14 @@ resource "aws_eip" "bastion_eip" {
     Module      = "04-bastion-host/01-eip"
   })
 }
+
+resource "aws_ssm_parameter" "bastion_ip" {
+  name        = "/${var.project_name}/04-bastion-host/01-eip/bastion-ip"
+  description = "Bastion Host Public IP"
+  type        = "String"
+  value       = aws_eip.bastion_eip.public_ip
+
+  tags = merge(var.common_tags, {
+    Module = "04-bastion-host/01-eip"
+  })
+}
