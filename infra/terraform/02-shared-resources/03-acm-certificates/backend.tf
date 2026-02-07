a352fb10717ae80674aeb6b55753d9b546bb1e18
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-07 21:39:12 +0100

    Update backend.tf

--- infra/terraform/02-shared-resources/03-acm-certificates/backend.tf
+++ infra/terraform/02-shared-resources/03-acm-certificates/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "modules/02-shared-resources/03-acm-certificates/terraform.tfstate"
     region         = "eu-south-2"
     encrypt        = true
-    dynamodb_table = "agevegacom-terraform-locks"
+    dynamodb_table = "terraform-state-lock"
     profile        = "terraform"
   }
 }
