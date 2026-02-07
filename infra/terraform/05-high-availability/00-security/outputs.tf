003b8c0d21d1018370e7e96c89fdf776d055e38e
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-07 21:12:52 +0100

    Update outputs.tf

--- infra/terraform/05-high-availability/00-security/outputs.tf
+++ infra/terraform/05-high-availability/00-security/outputs.tf
@@ -10,7 +10,7 @@ output "instance_sg_id" {
 
 output "instance_profile_name" {
   description = "IAM Instance Profile name for EC2 instances"
-  value       = aws_iam_instance_profile.app_profile.name
+  value       = aws_iam_instance_profile.ec2_profile.name
 }
 
 output "certificate_arn" {
