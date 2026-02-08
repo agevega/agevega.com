81f43c28739f7cdfc4934bc0d0d1b1a60de6e467
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-08 17:18:05 +0100

    Update main.tf

--- infra/terraform/04-bastion-host/04-cloudfront/main.tf
+++ infra/terraform/04-bastion-host/04-cloudfront/main.tf
@@ -32,10 +32,6 @@ resource "aws_cloudfront_distribution" "distribution" {
       origin_ssl_protocols   = ["TLSv1.2"]
     }
 
-    custom_header {
-      name  = "Host"
-      value = "dev.${var.domain_name}"
-    }
   }
 
   origin {
