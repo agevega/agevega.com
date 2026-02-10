8d7f280177a2a399165c75abf722a2de076dd294
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-10 19:32:23 +0100

    Update outputs.tf

--- infra/terraform/05-high-availability/04-dns-record/outputs.tf
+++ infra/terraform/05-high-availability/04-dns-record/outputs.tf
@@ -1,1 +1,9 @@
-# No outputs required for now
+output "root_url" {
+  description = "Production Environment URL"
+  value       = "https://${aws_route53_record.root.name}"
+}
+
+output "www_url" {
+  description = "Production Environment URL (WWW)"
+  value       = "https://${aws_route53_record.www.name}"
+}
