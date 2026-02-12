f00d11d8e912b0fcf814a6368883cee444ed19d1
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-12 13:56:01 +0100

    Update main.tf

--- infra/terraform/05-high-availability/00-security/main.tf
+++ infra/terraform/05-high-availability/00-security/main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "ingress_ssh_bastion" {
   to_port                  = 22
   protocol                 = "tcp"
   security_group_id        = aws_security_group.instances_sg.id
-  source_security_group_id = data.terraform_remote_state.bastion.outputs.security_group_id
+  source_security_group_id = data.terraform_remote_state.bastion.outputs.bastion_sg_id
 }
 
 resource "aws_security_group_rule" "ingress_https_alb" {
