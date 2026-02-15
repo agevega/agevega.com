e7af031ef82ae19e6d66942eb08d20285ee5af8f
Alejandro Vega Vaquero <agevega@gmail.com>
2026-02-15 19:40:37 +0100

    05/01 associate_public_ip_address = false

--- infra/terraform/05-high-availability/01-ec2-autoscaling/launch_template.tf
+++ infra/terraform/05-high-availability/01-ec2-autoscaling/launch_template.tf
@@ -9,7 +9,7 @@ resource "aws_launch_template" "app_lt" {
   }
 
   network_interfaces {
-    associate_public_ip_address = var.deploy_in_public_subnets
+    associate_public_ip_address = false
     security_groups             = [data.terraform_remote_state.security.outputs.instance_sg_id]
   }
 
