0f64276f00f45d734d98ca11e109c12fa86fcda1
Alejandro Vega <agevega@gmail.com>
2026-05-01 12:33:55 +0200

    Update astro.config.mjs

--- academy/astro.config.mjs
+++ academy/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://academy.agevega.com',
+  server: { port: 4322, host: true },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
